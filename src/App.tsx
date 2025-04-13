64b4b202da5324db09e3937dca1d043740347fa6
LucasVTK <lucazim.vtk@gmail.com>
2025-04-13 14:34:23 -0300

    Configurando o ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import Teste from './teste'
+import EstiloGlobal from './styles'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  )
+}
+
+export default App
